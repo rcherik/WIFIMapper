5b514840de0e0a1557ee7899f17604adae115dbc
mdufaud <mdufaud@student.42.fr>
2018-11-17 00:26:09 +0100

    Dynamic layout + panel + quality of life

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 #coding: utf-8
 
-# System
+""" System """
 from __future__ import print_function
 import os
 
-""" Force kivy not to interpret args """
+""" Forces kivy to not interpret args """
 os.environ['KIVY_NO_ARGS'] = "1"
 
 import argparse
@@ -13,7 +13,7 @@ import sys
 import time
 import threading
 from subprocess import Popen, PIPE
-# Kivy
+""" Kivy """
 from kivy.app import App, runTouchApp
 
 from kivy.config import Config
@@ -21,18 +21,22 @@ Config.set('graphics', 'width', '1280')
 Config.set('graphics', 'height', '800')
 
 from kivy.lang import Builder
+from kivy.core.window import Window
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
-from kivy.clock import Clock
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.scrollview import ScrollView
-from kivy.core.window import Window
-# Scapy
+from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelHeader
+from kivy.properties import ObjectProperty
+from kivy.clock import Clock
+""" Scapy """
 from scapy.all import sniff, rdpcap
 from scapy.error import Scapy_Exception
-# Our stuff
+""" Our Stuff """
 from wifi_mapper import parse_pkt
 
 Builder.load_file("build.kv")
@@ -46,10 +50,11 @@ class Thread(threading.Thread):
         self.iface = args.interface or self.find_iface()
         self.pcap_file = args.pcap or None
         self.pkts = pkts
-        self.pkt_idx = 0
+	self.dic = {}
+	self.old_dic = {'AP': {}, 'Station': {}, 'Traffic': {}}
 
     def find_iface(self):
-        """ Fine internet interface and returns it """
+        """ Find internet interface and returns it """
         try:
             dn = open("/dev/NULL", 'w')
         except IOError:
@@ -73,81 +78,130 @@ class Thread(threading.Thread):
 
     def callback(self, pkt):
         """ Callback when packet is sniffed """
-	if self.app and self.app.layout:
-            self.app.layout.parse_pkt(pkt)
+        parse_pkt(self.old_dic, pkt)
+        if self.app and hasattr(self.app, "managers"):
+            for manager in self.app.managers:
+                manager.screen.update_gui(self.old_dic)
 
-    def read_one(self):
-        """ Read one pkt from file """
-        pkt = self.pkts[self.pkt_idx]
-        self.pkt_idx += 1
-        self.callback(pkt)
+    def wait_for_gui(self):
+        """ Check if all screen are loaded """
+        gui_loaded = False
+        while not gui_loaded:
+            if self.app and hasattr(self.app, "managers"):
+                for manager in self.app.managers:
+                    if not manager.screen.ready:
+                        gui_loaded = False
+                        break
+                    else:
+                        gui_loaded = True
 
     def run(self):
         """ Thread either sniff or waits """
-        if self.pcap_file:
-            while not self.stop:
-                pass
-        else:
+        self.wait_for_gui()
+        print("Thread: screens are ready - Starts ", end="")
+        if not self.pcap_file:
+            print("sniffing")
 	    sniff(self.iface, prn=self.callback,
                     stop_filter=self.callback_stop, store=0)
+        else:
+            print("reading")
+            for pkt in self.pkts:
+                self.callback(pkt)
+                time.sleep(0.001)
+            print("Thread: has finished reading")
 
     def set_application(self, app):
         """ To be able to call Kivy from thread """
         self.app = app
 
-class MainLayout(StackLayout):
+class AppScreenManager(ScreenManager):
+
     def __init__(self, **kwargs):
-        """ If --pcap specified, will parse a pcap file """
-	super(MainLayout, self).__init__(**kwargs)
+        """ Add screen for thread accessibility """
+        super(AppScreenManager, self).__init__(**kwargs)
+        self.screen = ScrollScreen(**kwargs)
+        self.add_widget(self.screen)
+
+class ScrollScreen(Screen):
+
+    view = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        """ Delays view creation """
+	super(ScrollScreen, self).__init__(**kwargs)
+        self.ready = False
 	self.btn_dic = {}
-	self.dic = {}
-	self.old_dic = {'AP': {}, 'Station': {}, 'Traffic': {}}
-        self.thread = kwargs['thread']
-        self.thread.start()
-        if self.thread.pcap_file:
-            Clock.schedule_interval(self.pcap_read_file, 0.01)
+        self.show_ap = kwargs.get('ap', False)
+        self.show_station = kwargs.get('station', False)
+        Clock.schedule_once(self.create_view)
 
-    def pcap_read_file(self, clock):
-        """ Parse one packet from file """
-        self.thread.read_one()
-
-    def parse_pkt(self, pkt):
-        """ Parse one packet """
-        parse_pkt(self.old_dic, pkt)
-        self.update_gui()
-
-    def update_pkt(self, mac):
-	self.btn_dic[mac].text = pkt + '| ' + str(self.dic[mac])
-
-    def new_pkt(self, mac):
-	btn = Button(text=mac + '| 1', width=30, size_hint=(0.30, 0.30))
-	self.btn_dic[mac] = btn
-	self.add_widget(btn)
+    def create_view(self, dt):
+        """ Create layout """
+        self.layout = StackLayout(orientation="lr-tb",
+                                    size_hint_y=None)
+        self.layout.bind(minimum_height=self.layout.setter('height'))
+        self.view.add_widget(self.layout)
+        self.ready = True
 
     def add_btn(self, id, s="No name"):
-	btn = Button(text=s, width=30, size_hint=(0.30, 0.30))
-        self.btn_dic[id] = btn
-        self.add_widget(btn)
+        """ Add button or update it """
+        if id not in self.btn_dic:
+            btn = Button(text=s,
+                    size=(len(s) * 10, 50),
+                    size_hint=(None, None))
+            self.btn_dic[id] = btn
+            self.layout.add_widget(btn)
+        else:
+            self.btn_dic[id].text = s
+            self.btn_dic[id].size = (len(s) * 10, 50)
 
-    def update_gui(self):
-        """ Update packet on GUI """
-        ap = self.old_dic['AP']
-        sta = self.old_dic['Station']
-        for key, value in ap.iteritems():
-            self.add_btn(key, "AP: %s - %s" % (key, ap[key].ssid))
-        for key, value in sta.iteritems():
-            s = sta[key].recv if hasattr(sta[key], 'recv') else "None"
-            self.add_btn(key, "Sta: %s - %s" % (key, s))
+    def update_gui(self, dic):
+        """ Update GUI """
+        if self.show_ap:
+            ap = dic['AP']
+            for key, value in ap.iteritems():
+                self.add_btn(key, "AP: %s - %s" % (key, ap[key].ssid))
+        if self.show_station:
+            sta = dic['Station']
+            for key, value in sta.iteritems():
+                s = sta[key].get_probes()
+                self.add_btn(key, "Sta: %s - %s" % (key, s))
 
-class Wifi_mapper(App):
+class WifiMapper(App):
+
     def __init__(self, args, thread):
 	App.__init__(self)
-	self.thread = thread
-	self.layout = None
+        self.panel = None
+        self.managers = []
+        self.panel_lst = []
+        self.args = args
+        self.thread = thread
 
     def build(self):
-	self.layout = MainLayout(thread=self.thread)
-	return self.layout
+        """
+            Build a panel that contains AppScreenManagers
+
+            self.managers contains every AppScreenManagers
+            self.panel_lst contains every Panels
+        """
+	self.ap_manager = AppScreenManager(ap=True)
+	self.station_manager = AppScreenManager(station=True)
+        self.managers.append(self.ap_manager)
+        self.managers.append(self.station_manager)
+
+        self.panel= TabbedPanel()
+        self.panel.default_tab_text = "Access Points"
+        self.panel.default_tab_content = self.ap_manager
+        th_station = TabbedPanelHeader(text="Station")
+        th_station.content = self.station_manager
+
+        self.panel_lst.append(self.panel)
+        self.panel_lst.append(th_station)
+        self.panel.add_widget(th_station)
+
+        g_thread.start()
+
+	return self.panel
 
     def onstop(self):
         self.thread.stop = True
@@ -164,22 +218,22 @@ def parse_args():
 
 def read_pkts(name):
     """ Load pkts from file """
-    print("Reading file {}".format(name))
+    print("WifiMapper: reading file {}".format(name))
     start_time = time.time()
     try:
             packets = rdpcap(name)
     except (IOError, Scapy_Exception) as err:
-            print("Pcap parser error: {}".format(err),
+            print("rdpcap: error: {}".format(err),
                     file=sys.stderr)
             sys.exit(1)
     except NameError as err:
-            print("Pcap parser error: not a pcap file ({})".format(err),
+            print("rdpcap error: not a pcap file ({})".format(err),
                     file=sys.stderr)
             sys.exit(1)
     except KeyboardInterrupt:
             sys.exit(1)
     read_time = time.time()
-    print("Took {0:.3f} seconds".format(read_time - start_time))
+    print("WifiMapper: took {0:.3f} seconds".format(read_time - start_time))
     return packets
 
 if __name__ == '__main__':
@@ -190,9 +244,9 @@ if __name__ == '__main__':
     pkts = None
     if args.pcap:
         pkts = read_pkts(args.pcap)
-    thread = Thread(args, pkts)
-    app = Wifi_mapper(args, thread)
-    thread.set_application(app)
+    g_thread = Thread(args, pkts)
+    app = WifiMapper(args, g_thread)
+    g_thread.set_application(app)
 
     try:
         app.run()
@@ -200,8 +254,8 @@ if __name__ == '__main__':
         import traceback
         traceback.print_exc()
         print("Error : " + err.message)
-        thread.stop = True
-        thread.join()
+        g_thread.stop = True
+        g_thread.join()
         sys.exit(0) 
-    thread.stop = True
-    thread.join()
+    g_thread.stop = True
+    g_thread.join()
